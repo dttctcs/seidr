0fc6334ce94b81b187c1713f7fac3a357698bdd0
Mimameid <ibrahimkeser@outlook.de>
2022-07-04 06:58:16 +0200

    Updated seidr cli to include seidr-studio (WIP)

--- seidr/cli.py
+++ seidr/cli.py
@@ -1,11 +1,16 @@
 from io import BytesIO
 import os
+import shutil
 from urllib.request import urlopen
 from zipfile import ZipFile
+
 import click
 
-REPO_URL = (
+SEIDR_SKELETON_URL = (
     "https://github.com/dttctcs/seidr-skeleton/archive/master.zip"
+)
+SEIDR_STUDIO_URL = (
+    "https://github.com/dttctcs/seidr-studio/archive/master.zip"
 )
 
 
@@ -25,11 +30,40 @@ def create_app(name):
         Create a Skeleton application (needs internet connection to github)
     """
     try:
-        url = urlopen(REPO_URL)
-        dirname = "seidr-skeleton-main"
-        zipfile = ZipFile(BytesIO(url.read()))
-        zipfile.extractall()
-        os.rename(dirname, name)
+        # seidr skeleton
+        url = urlopen(SEIDR_SKELETON_URL)
+        skeleton_dirname = "seidr-skeleton-main"
+        skeleton_zipfile = ZipFile(BytesIO(url.read()))
+        skeleton_zipfile.extractall()
+        os.rename(skeleton_dirname, name)
+
+        # seidr studio
+        url = urlopen(SEIDR_STUDIO_URL)
+        studio_dirname = "seidr-studio-main"
+        studio_zipfile = ZipFile(BytesIO(url.read()))
+        for file in studio_zipfile.namelist():
+            if 'build' in file:
+                studio_zipfile.extract(file, name)
+
+        # templates and static folder
+        build_path = os.path.join(name, studio_dirname, 'build')
+        templates_path = os.path.join(name, 'app', 'templates')
+        os.mkdir(templates_path)
+        shutil.move(os.path.join(build_path, 'index.html'), templates_path)
+
+        static_path = os.path.join(name, 'app', 'static')
+        os.mkdir(static_path)
+        for file in os.listdir(build_path):
+            file_path = os.path.join(build_path, file)
+            if 'static' not in file_path:
+                shutil.move(file_path, static_path)
+            else:
+                for f in os.listdir(file_path):
+                    sub_path = os.path.join(file_path, f)
+                    shutil.move(sub_path, static_path)
+
+        shutil.rmtree(os.path.join(name, skeleton_dirname))
+
         click.echo(click.style("Downloaded the skeleton app. Happy coding!", fg="green"))
         return True
     except Exception as e:
