f397e1238ce85c5a64c452d7d6785718f399327f
Matthias Leinweber <m.leinweber@datatactics.de>
2023-06-13 20:34:27 +0200

    make special caracter filtering great again

--- seidr/interfaces/api.py
+++ seidr/interfaces/api.py
@@ -1,19 +1,86 @@
+import functools
+import json
+import urllib
+from typing import Any, Callable, Optional, Dict
+import jsonschema
+import prison as prison
 from flask_appbuilder.api import BaseApi, ModelRestApi, merge_response_func, expose, safe, rison, get_info_schema
 from flask_appbuilder._compat import as_unicode
 
 from flask_appbuilder.security.decorators import permission_name, protect
-
+from flask import  Response, request
 from flask_appbuilder.const import API_ADD_COLUMNS_RIS_KEY, API_ADD_TITLE_RIS_KEY, API_PERMISSIONS_RIS_KEY, \
-    API_EDIT_COLUMNS_RIS_KEY, API_FILTERS_RIS_KEY, API_EDIT_TITLE_RIS_KEY, API_FILTERS_RES_KEY
-
+    API_EDIT_COLUMNS_RIS_KEY, API_FILTERS_RIS_KEY, API_EDIT_TITLE_RIS_KEY, API_FILTERS_RES_KEY, \
+    API_ORDER_COLUMNS_RIS_KEY, API_LABEL_COLUMNS_RIS_KEY, API_DESCRIPTION_COLUMNS_RIS_KEY, API_LIST_COLUMNS_RIS_KEY, \
+    API_LIST_TITLE_RIS_KEY, API_URI_RIS_KEY
+from flask_appbuilder.api.schemas import get_list_schema
 from seidr.interfaces.convert import Model2SchemaConverter
 
 from copy import deepcopy
+def better_rison(
+        schema: Optional[Dict[str, Any]] = None
+) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
+    """
+    Use this decorator to parse URI *Rison* arguments to
+    a python data structure, your method gets the data
+    structure on kwargs['rison']. Response is HTTP 400
+    if *Rison* is not correct::
 
+        class ExampleApi(BaseApi):
+                @expose('/risonjson')
+                @rison()
+                def rison_json(self, **kwargs):
+                    return self.response(200, result=kwargs['rison'])
+
+    You can additionally pass a JSON schema to
+    validate Rison arguments::
+
+        schema = {
+            "type": "object",
+            "properties": {
+                "arg1": {
+                    "type": "integer"
+                }
+            }
+        }
+
+        class ExampleApi(BaseApi):
+                @expose('/risonjson')
+                @rison(schema)
+                def rison_json(self, **kwargs):
+                    return self.response(200, result=kwargs['rison'])
+
+    """
+
+    def _rison(f: Callable[..., Any]) -> Callable[..., Any]:
+        def wraps(self: "BaseApi", *args: Any, **kwargs: Any) -> Response:
+            value = request.args.get(API_URI_RIS_KEY, None)
+            kwargs["rison"] = dict()
+            if value:
+                try:
+                    kwargs["rison"] = prison.loads(value)
+                except prison.decoder.ParserException:
+                    try:
+                        kwargs["rison"] = json.loads(value)
+                    except Exception:
+                        return self.response_400(
+                            message="Not a valid rison/json argument"
+                        )
+            if schema:
+                try:
+                    jsonschema.validate(instance=kwargs["rison"], schema=schema)
+                except jsonschema.ValidationError as e:
+                    return self.response_400(message=f"Not a valid rison schema {e}")
+            return f(self, *args, **kwargs)
+
+        return functools.update_wrapper(wraps, f)
+
+    return _rison
 
 class SeidrApi(ModelRestApi):
     allow_browser_login = True
     quick_filters = None
+    filter_options = None
     search_model_schema = None
     search_query_rel_fields = None
     """
@@ -48,8 +115,9 @@ class SeidrApi(ModelRestApi):
         name = self.resource_name or self.__class__.__name__.lower()
         self.list_title = name.capitalize()
         self.quick_filters = self.quick_filters or []
+        self.filter_options = self.filter_options or []
         self.search_model_schema_name = f"{self.__class__.__name__}.search"
-        
+
         self.search_query_rel_fields = self.search_query_rel_fields or dict()
 
         if self.search_model_schema is None:
@@ -119,6 +187,21 @@ class SeidrApi(ModelRestApi):
 
         response["quickfilters"] = quick_filters
 
+    def merge_filter_options(self, response, **kwargs):
+        """
+        Overrides parent method to add the schema of a filter to the response. Selection is based on show columns.
+        :param response: The response object
+        :param kwargs: api endpoint kwargs
+        """
+
+        # Get possible search fields and all possible operations
+        search_suggestions = deepcopy(self.filter_options)
+        for qf in search_suggestions or []:
+            if callable(qf["options"]):
+                qf["options"] = qf["options"]()
+
+        response["search_suggestions"] = search_suggestions
+
     @expose("/_info", methods=["GET"])
     @protect()
     @safe
@@ -134,6 +217,7 @@ class SeidrApi(ModelRestApi):
     @merge_response_func(ModelRestApi.merge_edit_title, API_EDIT_TITLE_RIS_KEY)
     @merge_response_func(merge_relations_info, "relations")
     @merge_response_func(merge_quick_filters, "quickfilters")
+    @merge_response_func(merge_filter_options, "filter_options")
     def info(self, **kwargs):
         """ Endpoint that renders a response for CRUD REST meta data
         ---
@@ -191,3 +275,100 @@ class SeidrApi(ModelRestApi):
         """
         return self.info_headless(**kwargs)
 
+
+
+
+    @expose("/", methods=["GET"])
+    @protect()
+    @safe
+    @permission_name("get")
+    @better_rison(get_list_schema)
+    @merge_response_func(ModelRestApi.merge_order_columns, API_ORDER_COLUMNS_RIS_KEY)
+    @merge_response_func(ModelRestApi.merge_list_label_columns, API_LABEL_COLUMNS_RIS_KEY)
+    @merge_response_func(ModelRestApi.merge_description_columns, API_DESCRIPTION_COLUMNS_RIS_KEY)
+    @merge_response_func(ModelRestApi.merge_list_columns, API_LIST_COLUMNS_RIS_KEY)
+    @merge_response_func(ModelRestApi.merge_list_title, API_LIST_TITLE_RIS_KEY)
+    def get_list(self, **kwargs: Any) -> Response:
+        """Get list of items from Model
+        ---
+        get:
+          description: >-
+            Get a list of models
+          parameters:
+          - in: query
+            name: q
+            content:
+              application/json:
+                schema:
+                  $ref: '#/components/schemas/get_list_schema'
+          responses:
+            200:
+              description: Items from Model
+              content:
+                application/json:
+                  schema:
+                    type: object
+                    properties:
+                      label_columns:
+                        type: object
+                        properties:
+                          column_name:
+                            description: >-
+                              The label for the column name.
+                              Will be translated by babel
+                            example: A Nice label for the column
+                            type: string
+                      list_columns:
+                        description: >-
+                          A list of columns
+                        type: array
+                        items:
+                          type: string
+                      description_columns:
+                        type: object
+                        properties:
+                          column_name:
+                            description: >-
+                              The description for the column name.
+                              Will be translated by babel
+                            example: A Nice description for the column
+                            type: string
+                      list_title:
+                        description: >-
+                          A title to render.
+                          Will be translated by babel
+                        example: List Items
+                        type: string
+                      ids:
+                        description: >-
+                          A list of item ids, useful when you don't know the column id
+                        type: array
+                        items:
+                          type: string
+                      count:
+                        description: >-
+                          The total record count on the backend
+                        type: number
+                      order_columns:
+                        description: >-
+                          A list of allowed columns to sort
+                        type: array
+                        items:
+                          type: string
+                      result:
+                        description: >-
+                          The result from the get list query
+                        type: array
+                        items:
+                          $ref: '#/components/schemas/{{self.__class__.__name__}}.get_list'  # noqa
+            400:
+              $ref: '#/components/responses/400'
+            401:
+              $ref: '#/components/responses/401'
+            422:
+              $ref: '#/components/responses/422'
+            500:
+              $ref: '#/components/responses/500'
+        """
+
+        return self.get_list_headless(**kwargs)
